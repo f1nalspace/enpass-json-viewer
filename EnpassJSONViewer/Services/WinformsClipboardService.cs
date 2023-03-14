730f4a1edb1a645b676da2a7e9c127c0d0be841b
f1nalspace <finalspace@googlemail.com>
2023-03-14 18:56:46 +0100

    Fixed crash when pasting empty string to clipboard

--- EnpassJSONViewer/Services/WinformsClipboardService.cs
+++ EnpassJSONViewer/Services/WinformsClipboardService.cs
@@ -4,6 +4,10 @@ namespace EnpassJSONViewer.Services
 {
     class WinformsClipboardService : IClipboardService
     {
-        public void SetText(string text) => Clipboard.SetText(text ?? string.Empty);
+        public void SetText(string text)
+        {
+            if (!string.IsNullOrEmpty(text))
+                Clipboard.SetText(text);
+        }
     }
 }
